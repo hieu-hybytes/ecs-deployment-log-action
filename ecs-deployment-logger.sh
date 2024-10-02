ff603768e4dbd6351e94d5c8dc19913cf678373c
hieu <nghiem.hieu@hybytes.com>
2024-10-02 14:53:04 +0700

    Update function

--- ecs-deployment-logger.sh
+++ ecs-deployment-logger.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#
+# Get deployment status from Amazon ECS deployments
 
 CLUSTER="${1}"
 SERVICE="${2}"
@@ -8,8 +10,33 @@ output_cluster_events() {
   aws ecs describe-services --cluster "${CLUSTER}" --services "${SERVICE}" | jq -r '.services[0].events[].message'
 }
 
+#######################################
+# Get Amazon ECS service deployment id
+# Globals:
+#   CLUSTER
+#   SERVICE
+#   STATE
+# Agruments:
+#   None
+# Outputs:
+#   Write service deployment id to stdout
+#######################################
 get_deployment_ids() {
   aws ecs describe-services --cluster "${CLUSTER}" --services "${SERVICE}" --query "services[*].deployments[?rolloutState==\`${STATE}\`].id" --output text
+}
+
+#######################################
+# Get rollout state of a service deployment
+# Globals:
+#   CLUSTER
+#   SERVICE
+# Agruments:
+#   Service deployment id
+# Outputs:
+#   Write service deployment failure reason to stdout
+#######################################
+get_deployment_rollout_state() {
+  aws ecs describe-services --cluster "${CLUSTER}" --services "${SERVICE}" --query "services[*].deployments[?id==\`${1}\`].rolloutStateReason" --output text
 }
 
 get_task_ids() {
@@ -20,22 +47,13 @@ get_task_ids() {
     aws ecs list-tasks --cluster "${CLUSTER}" --no-paginate --started-by "${1}" ${opts} | jq '[.taskArns[] | split("/") | .[2]]'
 }
 
-get_task() {
-  aws ecs describe-tasks --cluster "${CLUSTER}" --tasks "${1}" | jq '.tasks[0]'
-}
-
 get_task_stopped_reason() {
   aws ecs describe-tasks --cluster "${CLUSTER}" --tasks "${1}" --query 'tasks[0].stoppedReason'
 }
 
 process_task() {
-
     REASON=$(get_task_stopped_reason "${1}")
-    # leaves dummy values
-    TASK=$(get_task "${1}")
-
-    typeset -p TASK \
-	       REASON 
+    echo "${REASON}"
 }
 
 process_deployment() {
@@ -52,15 +70,20 @@ process_deployment() {
 
 process_tasks() {
   for taskId in $(echo "${1}" | jq -r '.[]'); do
-    echo process_task "${taskId}"
+    echo "Processing task ${taskId}"
     process_task "${taskId}"
+    return
   done
 }
 
 main() {
     SERVICE_DEPLOYMENT_ID=$(get_deployment_ids)
+    ROLLOUT_STATE_REASON=$(get_deployment_rollout_state "${SERVICE_DEPLOYMENT_ID}")
+
+    typeset -p SERVICE_DEPLOYMENT_ID \
+	    ROLLOUT_STATE_REASON
+
     process_deployment "${SERVICE_DEPLOYMENT_ID}"
 }
 
 main
-
